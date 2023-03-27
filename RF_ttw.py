ad217fbda2a25e7af1d1e03ef29006c627cc56f4
javier del riego <UO257479@uniovi.es>
2023-03-27 17:36:07 +0200

    Prepared to optimize hyperparameters in the RF case

--- RF_ttw.py
+++ RF_ttw.py
@@ -1,4 +1,3 @@
-''' Implementation of a neural network to discriminate ttW ''' #just trying
 # -- Import libraries 
 import pandas as pd
 import numpy as np
@@ -7,6 +6,7 @@ import df_utils as dfu
 from samples import smp2df
 import matplotlib.pyplot as plt
 from functions import *
+from grid import *
 
 # -- ML libraries 
 from sklearn.metrics import confusion_matrix
@@ -22,13 +22,13 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix, auc, precision_recall_curve
 from tensorflow import keras
 from sklearn import tree, ensemble
+import xgboost as xgb
 
 import time
 start_time = time.time()
 
-#pd.set_option('display.max_rows',10)
+#pd.set_option('display.max_rows',None)
 #pd.set_option('display.max_columns',None)
-
 
 
 def roc_auc_plot(y_true, y_proba, label=' ', l='-', lw=1.0):
@@ -38,6 +38,9 @@ def roc_auc_plot(y_true, y_proba, label=' ', l='-', lw=1.0):
 
 
 if __name__ == "__main__":
+	
+###########################					BUILDING THE DATAFRAME												##############################	
+	
     mainpath = "/beegfs/data/nanoAODv9/ttW_data_forFlips/mva_vars_v2/"#"/beegfs/data/TOPnanoAODv6/ttW_MC_Ntuples_skim/"
 
     # Dictionary with sample names
@@ -85,7 +88,8 @@ if __name__ == "__main__":
       smp_ttw.load_data(                   #loaddata en samples.py
         path = mainpath + "%s"%year, 
         selection = "nLepGood==2",  #&(JetSel_Recl_btagDeepFlavB[0]>=0.5)",    #Selection to add in case we deal with certain requirement (for instance if we add a variable on jets, we must ask for having at least one jet)
-        stop=1000000,
+        #stop=1000000,
+        stop=21000,
         process = samples["ttW"][year]) 
     smp_ttw.label_dataframe(val = 1)   #etiquetado (señal 1 bck 0)
 
@@ -97,7 +101,8 @@ if __name__ == "__main__":
       smp_tt.load_data(
         path = mainpath + "%s"%year, 
         selection = "nLepGood==2",#"nJet25_Recl<=5",
-        stop=1000000,
+        #stop=1000000,
+        stop=80000,
         process = samples["ttbar"][year]) 
     smp_tt.label_dataframe(val = 0)   #check that since this is bckg, val must be 0
     
@@ -106,16 +111,18 @@ if __name__ == "__main__":
 
     print(df_ttw)
     
-    ###############################Definicion variables 'complejas' 
+   
     
-    
-    
-    
-    
+  
     # Combinamos todos los dataframes
     dfs_to_combine = [df_ttw, df_ttbar]
     df = dfu.combine_dataframes(dfs_to_combine, axis = 0) # Para concatenar filas
     
+    
+    
+  
+  
+    ###############################      COMPLEX VARIABLE DEFINITION TO ADD TO THE 'SIMPLE' DATAFRAME  					############################  
     
     ######		B-tagging business #####
     df=df.assign(B1_pt=100)
@@ -128,12 +135,9 @@ if __name__ == "__main__":
     df=df.assign(B2_mass=100)
     
     
-    btagging(df,workingpoint="Loose")
+   # btagging(df,workingpoint="Tight")
     
     
-    
-    
-    ############################### Complex variable definition
     
     # Create 4 vectors
     l1 = create4vec(df["lep1_pt"], df["lep1_eta"], df["lep1_phi"], df["lep1_mass"])
@@ -142,7 +146,7 @@ if __name__ == "__main__":
     j1 = create4vec(df["jet1_pt"], df["jet1_eta"], df["jet1_phi"], df["jet1_mass"])
     j2 = create4vec(df["jet2_pt"], df["jet2_eta"], df["jet2_phi"], df["jet2_mass"])
     
-    #b1=create4vec(df["B1_pt"],df["B1_eta"],df["B1_phi"],df["B1_mass"])
+   # b1=create4vec(df["B1_pt"],df["B1_eta"],df["B1_phi"],df["B1_mass"])
     #b2=create4vec(df["B2_pt"],df["B2_eta"],df["B2_phi"],df["B2_mass"])
     
     
@@ -168,7 +172,7 @@ if __name__ == "__main__":
     #df["deltaetalep"]=deltaeta(l1,l2)
     #df["deltaphilep"]=deltaphi(l1,l2)
     #df["deltarlep"]=deltar(l1,l2)
-    #df["deltarjet"]=deltar(j1,j2)
+   # df["deltarjet"]=deltar(j1,j2)     #dejar para xgb
     
     
     
@@ -177,7 +181,7 @@ if __name__ == "__main__":
     #df["deltarlb21"]=deltar(l2,b1)
     #df["deltarlb22"]=deltar(l2,b2)
     
-    #df["deltarjb11"]=deltar(j1,b1)
+   # df["deltarjb11"]=deltar(j1,b1)    #dejar para xgb
     #df["deltarjb12"]=deltar(j1,b2)
     #df["deltarjb21"]=deltar(j2,b1)
     #df["deltarjb22"]=deltar(j2,b2)
@@ -194,61 +198,40 @@ if __name__ == "__main__":
     
     
     
+ ########################################				STARTING OF THE MACHINE LEARNING MODELS						#############################   
     
-    #Variables to add to the training
-    vars_train = ["year","htJet25j_Recl","jet2_pt","nJet25_Recl","jet3_pt","jet2_mass","lep2_pt","jet3_btagDeepFlavB",
+      
+    #Variables to add to the training. We can split a diferent selection of variables for each model:
+   
+    vars_train_RF = ["year","htJet25j_Recl","jet2_pt","nJet25_Recl","jet3_pt","jet2_mass","lep2_pt","jet3_btagDeepFlavB",
      "lep1_pt","mlj11","jet3_mass","jet1_pt","jet4_mass","lep1_charge","lep2_mass","jet4_pt","jet3_eta","jet3_phi",
-     'jet1_mass','jet4_btagDeepFlavB','Flav_muon','jet4_phi','mll','lep2_elec','jet4_eta','mlj12',"jet5_eta","Flav_elec",'mlj21','mlj22',"combipt","lep2_charge",   #aprox hasta aqui bn para RF
-     'jet2_phi','notBjets','jet2_btagDeepFlavB', 'jet5_pt', 'jet2_eta', 'nBJetLoose25_Recl', 'MET_pt',
-     "B2_pt",'B1_mass', 'B1_pt', 'B2_mass',"B1_eta","B2_eta"]   #Up to here ok for the NN
+     'jet1_mass','jet4_btagDeepFlavB','Flav_muon','jet4_phi','mll','lep2_elec','jet4_eta','mlj12',"jet5_eta","Flav_elec",'mlj21','mlj22',"combipt","lep2_charge"]   #Up to here ok for the NN
      
      
-     #'lep1_mass', 'jet5_mass','jet5_btagDeepFlavB','nBJetMedium40_Recl','nBJetLoose40_Recl', 'Flav_mix', 'B2_eta','lep1_elec',
-     #'nBJetMedium25_Recl','jet5_phi', 'jet6_phi','jet1_btagDeepFlavB','jet6_pt', 'B1_eta',
-     #'jet1_eta','jet7_eta','jet6_mass','lep1_eta','lep2_eta','jet6_btagDeepFlavB','B2_phi','jet6_eta','B1_phi','lep1_phi','jet1_phi']
      
+     #		Splitting of datasets (for each of the training vars subset)
      
-    #vars_train=["notBjets","deltaetalep","deltaphilep","deltarjet","deltarlep","combipt","mll","year",
-     #                    "deltaphilj11","deltaphilj12","deltaphilj21","deltaphilj22",
-      #                   "deltarlj11","deltarlj12","deltarlj21","deltarlj22",
-       #                  "deltarlb11","deltarlb12","deltarlb21","deltarlb22",
-        #                 "deltarjb11","deltarjb12","deltarjb21","deltarjb22",
-         #                "mlj11","mlj12","mlj21","mlj22",
-          #               "Flav_elec","Flav_muon","Flav_mix",
-           #              "B1_pt","B1_eta","B1_phi","B1_mass",
-            #             "B2_pt","B2_eta","B2_phi","B2_mass",
-             #            "year", 
-              #           "nLepGood", 
-               #          "lep1_pt", "lep1_eta","lep1_phi","lep1_mass","lep1_charge","lep1_elec",
-                #         "lep2_pt", "lep2_eta","lep2_phi","lep2_mass","lep2_elec","lep2_charge",
-                 #        "nJet25_Recl", 
-                  #       "htJet25j_Recl", 
-                   #      "MET_pt", 
-                    #     "nBJetLoose25_Recl",
-                     #    "nBJetMedium25_Recl",
-                      #   "nBJetLoose40_Recl",
-                       #  "nBJetMedium40_Recl",
-                        # "jet1_pt", "jet1_eta","jet1_phi","jet1_mass","jet1_btagDeepFlavB", 
-                         #"jet2_pt", "jet2_eta","jet2_phi","jet2_mass","jet2_btagDeepFlavB",
-                         #"jet3_pt", "jet3_eta","jet3_phi","jet3_mass","jet3_btagDeepFlavB",
-                         #"jet4_pt", "jet4_eta","jet4_phi","jet4_mass","jet4_btagDeepFlavB",
-                         #"jet5_pt", "jet5_eta","jet5_phi","jet5_mass","jet5_btagDeepFlavB",
-                         #"jet6_pt", "jet6_eta","jet6_phi","jet6_mass","jet6_btagDeepFlavB",
-                         #"jet7_pt", "jet7_eta","jet7_phi","jet7_mass","jet7_btagDeepFlavB"]
+    X_train_RF, X_test_RF, y_train_RF, y_test_RF = train_test_split(df[vars_train_RF], df['is_signal'], test_size=0.3, random_state=1)
+    df_train_RF=pd.concat([X_train_RF,y_train_RF], axis=1)
+    df_test_RF=pd.concat([X_test_RF,y_test_RF], axis=1)
     
+    X_test_RF, X_validation_RF, y_test_RF, y_validation_RF = train_test_split(df_test_RF[vars_train_RF], df_test_RF['is_signal'], test_size=0.8, random_state=5)
+    df_test_RF=pd.concat([X_test_RF,y_test_RF], axis=1)
+    df_validation_RF=pd.concat([X_validation_RF,y_validation_RF], axis=1)
     
-    # From Andrea
-    X_train, X_test, y_train, y_test = train_test_split(df[vars_train], df['is_signal'], test_size=0.3, random_state=1)
-    df_train=pd.concat([X_train,y_train], axis=1)
-    df_test=pd.concat([X_test,y_test], axis=1)
+    bucle_RF(X_train_RF,y_train_RF,X_test_RF,y_test_RF,df_test_RF,df_train_RF,vars_train_RF)
     
-    X_test, X_validation, y_test, y_validation = train_test_split(df_test[vars_train], df_test['is_signal'], test_size=0.8, random_state=5)
-    df_test=pd.concat([X_test,y_test], axis=1)
-    df_validation=pd.concat([X_validation,y_validation], axis=1)
+    #gboost = xgb.XGBClassifier(n_estimators=100, max_depth=3, learning_rate=0.01, n_jobs=-1,use_label_encoder=False)#ensemble.GradientBoostingClassifier(n_jobs=-1,max_depth=15, min_samples_leaf=5)				#Descomentar si quiero comparar con gradientboosting (pero solo se puede correr con pocos datos, boosting no paraleliza bien)
+    #gboost.fit(X_train_gboost,y_train_gboost)  #Training of the GradBoost
     
+    #print("Gboost AUC (test) = {0}".format(roc_auc_score(df_test_gboost['is_signal'],gboost.predict_proba(df_test_gboost[vars_train_gboost])[:,1])))
+    #print("Gboost AUC (train) = {0}".format(roc_auc_score(df_train_gboost['is_signal'],gboost.predict_proba(df_train_gboost[vars_train_gboost])[:,1])))
     
-    RF=RandomForestClassifier(n_jobs=-1,min_samples_leaf=1000,max_depth=1/3*len(vars_train),min_samples_split=2)
-    RF.fit(X_train,y_train)  #Trainig of the RF
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+
+    print("Elapsed time:", elapsed_time/60, "minutes") 
+    exit()
     
     
     
@@ -256,32 +239,29 @@ if __name__ == "__main__":
     
     
     
-    model = keras.models.Sequential()
-    normal_ini=keras.initializers.glorot_normal(seed=None)
-    model.add(keras.layers.Dense(64,  input_shape = (len(vars_train),), activation='relu'))
-    model.add(keras.layers.Dense(32, activation='relu'))
-    model.add(keras.layers.Dense(16, activation='relu'))
-    model.add(keras.layers.Dense(16, activation='relu'))
-    model.add(keras.layers.Dense(8, activation='relu'))
-    model.add(keras.layers.Dense(2, activation='softmax'))
-    model.summary()
+	
+	
+    
+    #bucle(X_train_gboost,y_train_gboost,X_test_gboost,y_test_gboost,df_test_gboost,df_train_gboost,vars_train_gboost)
+    exit()
+    
+		
+		#gboost=xgb.XGBClassifier(n_estimators=100,max_depth=3,learning_rate=i,n_jobs=-1,use_label_encoder=False)
+		#gboost.fit(X_train_gboost,y_train_gboost)
+		#print("Learning rate={0}".format(i))
+		#print("Gboost AUC (test) = {0}".format(roc_auc_score(df_test_gboost['is_signal'],gboost.predict_proba(df_test_gboost[vars_train_gboost])[:,1])))
+		#print("Gboost AUC (train) = {0}".format(roc_auc_score(df_train_gboost['is_signal'],gboost.predict_proba(df_train_gboost[vars_train_gboost])[:,1])))
+		
+		
+		
     
     
     
+    ###############################						PLOTTING AND PRINTING RESULTS						##################################
     
-    sgd = keras.optimizers.SGD(lr=0.01)
-    #adamax=keras.optimizers.Adamax(learning_rate=0.02, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0)
-    adam=keras.optimizers.Adam(lr=0.0005)
-    model.compile(loss='binary_crossentropy', optimizer=adam, metrics = ["accuracy"])
-    histObj = model.fit(df_train[vars_train], keras.utils.to_categorical(df_train["is_signal"]), epochs=35, batch_size=1000,shuffle=True,validation_data=(df_validation[vars_train], keras.utils.to_categorical(df_validation["is_signal"])))
-    
-    
-    
-    
-    
-    train=df_train    							#Code for printing the relative importance of variables
-    features_list = train.columns.values
-    feature_importance = RF.feature_importances_
+    #VARIABLE IMPORTANCE
+    features_list = df_train_gboost.columns.values
+    feature_importance = gboost.feature_importances_
     sorted_idx = np.argsort(feature_importance)[::-1][:20]
     sorted_idx_full=np.argsort(feature_importance)
     plt.figure(figsize=(13,10))
@@ -289,56 +269,20 @@ if __name__ == "__main__":
     plt.yticks(range(len(sorted_idx)), features_list[sorted_idx][::-1])
     plt.xlabel('Importance')
     plt.title('Feature importances')
-    plt.savefig('importancia_variables_RF.png')
-    #print(features_list[sorted_idx_full],feature_importance[sorted_idx_full])
+    plt.savefig('importancia_variables_Gboost.png')
     
     
-   
-   
+    #AUCs
+    print("Gboost AUC (test) = {0}".format(roc_auc_score(df_test_gboost['is_signal'],gboost.predict_proba(df_test_gboost[vars_train_gboost])[:,1])))
+    print("Gboost AUC (train) = {0}".format(roc_auc_score(df_train_gboost['is_signal'],gboost.predict_proba(df_train_gboost[vars_train_gboost])[:,1])))
     
     
+    ##Plotting the AUCs
+    f, ax = plt.subplots(figsize=(7,8))   #Medida del AUC (indicador cuan bueno es el modelo)
     
-    ####Prediccion sobre test
-    from sklearn.metrics import roc_curve
-    from sklearn.metrics import roc_auc_score
-    import matplotlib.pyplot as plt
-    print("Running on test sample. This may take a moment.")
-    probs = model.predict(df_test[vars_train])#predict probability over test sample
-    AUC = roc_auc_score(df_test["is_signal"], probs[:,1])
-    print("Test Area under Curve = {0}".format(AUC))
+    roc_auc_plot(y_test_gboost,gboost.predict_proba(X_test_gboost),label='Gboost test',l='-')						
+    roc_auc_plot(y_train_gboost,gboost.predict_proba(X_train_gboost),label='Gboost train',l='-')
     
-    ######Prediccion sobre train
-    print("Running on train sample. This may take a moment.")
-    probs2 = model.predict(df_train[vars_train])#predict probability over train sample
-    AUC2 = roc_auc_score(df_train["is_signal"], probs2[:,1])
-    print("Train Area under Curve = {0}".format(AUC2))
-    
-    print("RF AUC={0}".format(roc_auc_score(df_test['is_signal'],RF.predict_proba(df_test[vars_train])[:,1])))
-    
-    plotLearningCurves(histObj)
-    
-    
-    #gboost = ensemble.GradientBoostingClassifier(max_depth=15, min_samples_leaf=5)				#Descomentar si quiero comparar con gradientboosting (pero solo se puede correr con pocos datos, boosting no paraleliza bien)
-    #gboost.fit(X_train,y_train)
-    #feature_importance = gboost.feature_importances_
-    #sorted_idx = np.argsort(feature_importance)[:20]
-    #plt.figure(figsize=(10,7))
-    #plt.barh(range(len(sorted_idx)), feature_importance[sorted_idx], align='center')
-    #plt.yticks(range(len(sorted_idx)), features_list[sorted_idx])
-    #plt.xlabel('Importance')
-    #plt.title('Feature importances')
-    #plt.savefig('importancia_variables_Gboost.png')
-    
-    
-    
-    
-    
-    f, ax = plt.subplots(figsize=(7,8))   #Medida del AUC (indicador cuan bueno es el modelo) (6,6 default)
-    roc_auc_plot(y_test,RF.predict_proba(X_test),label='FOREST',l='--')
-    roc_auc_plot(y_train,RF.predict_proba(X_train),label='Forest train')
-    #roc_auc_plot(y_test,gboost.predict_proba(X_test),label='GBOOST',l='-')						#Descomentar si gboost
-    roc_auc_plot(y_test,model.predict(df_test[vars_train]),label='NN',l='-.')
-    roc_auc_plot(y_train,model.predict(df_train[vars_train]),label='NN train')
     ax.plot([0,1], [0,1], color='k', linewidth=0.5, linestyle='--', label='Random Classifier')    
     ax.legend(loc="lower right")    
     ax.set_xlabel('False Positive Rate')
@@ -346,9 +290,5 @@ if __name__ == "__main__":
     ax.set_xlim([0, 1])
     ax.set_ylim([0, 1])
     ax.set_title('Receiver Operator Characteristic curves')
-    f.savefig('auc.png')    
+    f.savefig('auc_gboost.png') 
 
-end_time = time.time()
-elapsed_time = end_time - start_time
-
-print("Elapsed time:", elapsed_time/60, "minutes")
